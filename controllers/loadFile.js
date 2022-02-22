e29f5722b7c2140a6b992b1c103a76914f547ba3
John Serrano <andrey_201015@hotmail.com>
2022-02-22 15:35:59 -0500

    add and changes load file test

--- controllers/loadFile.js
+++ controllers/loadFile.js
@@ -9,31 +9,43 @@ function loadFile(req, res) {
   if (!req.files) return res.status(400).send({ message: 'No files were uploaded.', status: '400' })
 
   let fileXml = req.files.fileXml
+  const option = req.body.option
+  // console.log(req.body.option)
 
-  if (req.body.option === 'Alcaldia') {
+  if (option === 'Alcaldia') {
     fileXml.mv(path.resolve(__dirname, '../uploads/fileAlcaldia.xml'), function (err) {
       if (err) return res.status(500).send({ message: 'Algo salio mal ' + err, status: '500' })
 
       return res.status(200).send({ message: 'Se cargo el archivo correctamente.', status: '200' })
     })
-  } else if (req.body.option === 'Gobernacion') {
+  } else if (option === 'Gobernacion') {
     fileXml.mv(path.resolve(__dirname, '../uploads/fileGobernacion.xml'), function (err) {
       if (err) return res.status(500).send({ message: 'Algo salio mal ' + err, status: '500' })
 
       return res.status(200).send({ message: 'Se cargo el archivo correctamente.', status: '200' })
     })
-  } else if (req.body.option === 'Asamblea') {
+  } else if (option === 'Asamblea') {
     fileXml.mv(path.resolve(__dirname, '../uploads/fileAsamblea.xml'), function (err) {
       if (err) return res.status(500).send({ message: 'Algo salio mal ' + err, status: '500' })
 
       return res.status(200).send({ message: 'Se cargo el archivo correctamente.', status: '200' })
     })
-  } else if (req.body.option === 'Concejo') {
+  } else if (option === 'Concejo') {
     fileXml.mv(path.resolve(__dirname, '../uploads/fileConcejo.xml'), function (err) {
       if (err) return res.status(500).send({ message: 'Algo salio mal ' + err, status: '500' })
 
       return res.status(200).send({ message: 'Se cargo el archivo correctamente.', status: '200' })
     })
+  } else if (option === 'departamentalTest') {
+    fileXml.mv(path.resolve(__dirname, '../uploads/fileDepartamentalTest.xml'), function (err) {
+      if (err) return res.status(500).send({ message: 'Algo salio mal ' + err, status: '500' })
+
+      return res.status(200).send({ message: 'Se cargo el archivo correctamente.', status: '200' })
+    })
+  } else {
+    return res
+      .status(200)
+      .send({ message: 'No encontro una de las opciones para cargar el archivo.', status: '200' })
   }
 }
 
@@ -82,14 +94,17 @@ function jsonConcejoHttp(req, res) {
 }
 
 function jsonDepartamentalHttp(req, res) {
-  fs.readFile(path.resolve(__dirname, '../uploads/fileDepartamental.xml'), function (err, data) {
-    if (err) {
-      return res.status(500).send({ status: '500', message: 'algo salio mal departamental' })
+  fs.readFile(
+    path.resolve(__dirname, '../uploads/fileDepartamentalTest.xml'),
+    function (err, data) {
+      if (err) {
+        return res.status(500).send({ status: '500', message: 'algo salio mal departamental' })
+      }
+      const json = parser.toJson(data, { object: true })
+      // console.log('to json ->', json)
+      return res.status(200).send({ status: '200', data: json })
     }
-    const json = parser.toJson(data, { object: true })
-    // console.log('to json ->', json)
-    return res.status(200).send({ status: '200', data: json })
-  })
+  )
 }
 
 module.exports = {
